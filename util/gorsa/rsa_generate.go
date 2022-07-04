05e9ff8988a1f21d5566747dcfecae84270320e0
Gai <mteznja4ma@gmail.com>
2022-07-04 14:11:43 +0800

    RSA 私钥添加 PKCS8

--- util/gorsa/rsa_generate.go
+++ util/gorsa/rsa_generate.go
@@ -4,18 +4,24 @@ import (
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
-	"encoding/asn1"
 	"encoding/pem"
-	"log"
 )
 
-func GenerateRSAKey(bits int) (private, public []byte, err error) {
+func GenerateRSAKey(bits int, keyType PriKeyType) (private, public []byte, err error) {
 	// 生成私钥文件
 	privateKey, err := rsa.GenerateKey(rand.Reader, bits)
 	if err != nil {
 		panic(err)
 	}
-	derStream := x509.MarshalPKCS1PrivateKey(privateKey)
+	var derStream []byte
+	if keyType == PKCS8 {
+		derStream, err = x509.MarshalPKCS8PrivateKey(privateKey)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		derStream = x509.MarshalPKCS1PrivateKey(privateKey)
+	}
 	block := &pem.Block{
 		Type:  "RSA PRIVATE KEY",
 		Bytes: derStream,
@@ -32,42 +38,4 @@ func GenerateRSAKey(bits int) (private, public []byte, err error) {
 	}
 	public = pem.EncodeToMemory(block)
 	return
-}
-
-// 由私钥获取PKCS8公钥 这种方式生成的PKCS8与OpenSSL转成的不一样，但是BouncyCastle里可用
-func MarshalPKCS8PrivateKey(key []byte) []byte {
-	info := struct {
-		Version             int
-		PrivateKeyAlgorithm []asn1.ObjectIdentifier
-		PrivateKey          []byte
-	}{}
-	info.Version = 0
-	info.PrivateKeyAlgorithm = make([]asn1.ObjectIdentifier, 1)
-	info.PrivateKeyAlgorithm[0] = asn1.ObjectIdentifier{1, 2, 840, 113549, 1, 1, 1}
-	info.PrivateKey = key
-
-	k, err := asn1.Marshal(info)
-	if err != nil {
-		log.Panic(err.Error())
-	}
-	return k
-}
-
-// 由私钥获取PKCS8公钥
-func MarshalPKCS8PrivateKey1(key *rsa.PrivateKey) []byte {
-	info := struct {
-		Version             int
-		PrivateKeyAlgorithm []asn1.ObjectIdentifier
-		PrivateKey          []byte
-	}{}
-	info.Version = 0
-	info.PrivateKeyAlgorithm = make([]asn1.ObjectIdentifier, 1)
-	info.PrivateKeyAlgorithm[0] = asn1.ObjectIdentifier{1, 2, 840, 113549, 1, 1, 1}
-	info.PrivateKey = x509.MarshalPKCS1PrivateKey(key)
-
-	k, err := asn1.Marshal(info)
-	if err != nil {
-		log.Panic(err.Error())
-	}
-	return k
 }
