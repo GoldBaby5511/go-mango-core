23d105e4b342b5f7cb3959bbd65913a650ee4bef
goldbaby <mrallen5511@sina.com>
2022-07-26 10:38:00 +0800

    心跳增加时间戳字段

--- api/proto/build.bat
+++ api/proto/build.bat
@@ -1,14 +1,8 @@
 echo "go proto"
 @.\protoc.exe --go_out=.. gateway.proto
 @.\protoc.exe --go_out=plugins=grpc:.. center.proto
-@.\protoc.exe --go_out=.. login.proto
 @.\protoc.exe --go_out=.. types.proto
 @.\protoc.exe --go_out=.. logger.proto
 @.\protoc.exe --go_out=.. config.proto
-@.\protoc.exe --go_out=.. logic.proto
-@.\protoc.exe --go_out=.. battle.proto
-@.\protoc.exe --go_out=.. property.proto
-@.\protoc.exe --go_out=.. gameddz.proto
-@.\protoc.exe --go_out=.. gameAnkots.proto
 
 pause
