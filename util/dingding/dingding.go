af895a34b4767321e61d056c32cc3d21619484fa
goldbaby <mrallen5511@sina.com>
2022-08-31 11:50:35 +0800

    钉钉告警增加markdown格式

--- util/dingding/dingding.go
+++ util/dingding/dingding.go
@@ -1,17 +1,16 @@
 package dingding
 
-
 import (
-"bytes"
-"crypto/hmac"
-"crypto/sha256"
-"encoding/base64"
-"encoding/json"
-"errors"
-"fmt"
-"io/ioutil"
-"net/http"
-"time"
+	"bytes"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
 )
 
 type Webhook struct {
@@ -23,7 +22,7 @@ type Webhook struct {
 
 // SendMessage Function to send message
 //goland:noinspection GoUnhandledErrorResult
-func (t *Webhook) SendMessage(s string, at ...string) error {
+func (t *Webhook) SendTextMessage(s string, at ...string) error {
 	msg := map[string]interface{}{
 		"msgtype": "text",
 		"text": map[string]string{
@@ -49,6 +48,40 @@ func (t *Webhook) SendMessage(s string, at ...string) error {
 			return errors.New("the parameter \"EnableAt\" is \"false\", but the \"at\" parameter of SendMessage is not empty")
 		}
 	}
+	return t.sendMessage(msg)
+}
+
+func (t *Webhook) SendMarkdownMessage(s string, at ...string) error {
+	msg := map[string]interface{}{
+		"msgtype": "markdown",
+		"markdown": map[string]string{
+			"title": "告警消息",
+			"text":  s,
+		},
+	}
+	if t.EnableAt {
+		if t.AtAll {
+			if len(at) > 0 {
+				return errors.New("the parameter \"AtAll\" is \"true\", but the \"at\" parameter of SendMessage is not empty")
+			}
+			msg["at"] = map[string]interface{}{
+				"isAtAll": t.AtAll,
+			}
+		} else {
+			msg["at"] = map[string]interface{}{
+				"atMobiles": at,
+				"isAtAll":   t.AtAll,
+			}
+		}
+	} else {
+		if len(at) > 0 {
+			return errors.New("the parameter \"EnableAt\" is \"false\", but the \"at\" parameter of SendMessage is not empty")
+		}
+	}
+	return t.sendMessage(msg)
+}
+
+func (t *Webhook) sendMessage(msg map[string]interface{}) error {
 	b, err := json.Marshal(msg)
 	if err != nil {
 		return err
